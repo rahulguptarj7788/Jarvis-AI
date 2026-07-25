ec857b7da5d8934f068a1f828f0bb9f1752838b4
rahulguptarj7788 <rahulguptarj7788@gmail.com>
2026-07-25 08:52:25 +0530

    Update CommandAction.kt

--- app/src/main/java/com/voiceassistant/model/CommandAction.kt
+++ app/src/main/java/com/voiceassistant/model/CommandAction.kt
@@ -1,9 +1,9 @@
 package com.voiceassistant.model
 
 sealed class CommandAction {
-    data class LaunchApp(val packageName: String) : CommandAction()
-    data class Scroll(val direction: ScrollDirection) : CommandAction()
+    data class OpenApp(val appName: String) : CommandAction()
+    data class Scroll(val direction: Direction) : CommandAction() {
+        enum class Direction { UP, DOWN }
+    }
     data class TypeText(val text: String) : CommandAction()
 }
-
-enum class ScrollDirection { UP, DOWN }
