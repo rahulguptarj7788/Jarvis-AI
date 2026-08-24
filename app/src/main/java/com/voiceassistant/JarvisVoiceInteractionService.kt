6340b7e8cf40847fe13ba42a56d5366ff1525c76
rahulguptarj7788 <rahulguptarj7788@gmail.com>
2026-08-24 17:37:27 +0530

    Create JarvisVoiceInteractionService.kt

---
+++ app/src/main/java/com/voiceassistant/JarvisVoiceInteractionService.kt
@@ -0,0 +1,10 @@
+package com.voiceassistant
+
+import android.service.voice.VoiceInteractionService
+
+class JarvisVoiceInteractionService : VoiceInteractionService() {
+    override fun onReady() {
+        super.onReady()
+    }
+}
+
